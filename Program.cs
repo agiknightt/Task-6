b078db6cf096f6f4b95f14d0919641494661819d
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-31 16:14:48 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string zodiac;
+            string job;
+
+            Console.Write("Введите ваше имя : ");            
+            name = Console.ReadLine();
+
+            Console.Write("Сколько вам лет? : ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Какой ваш знак зодиака? : ");
+            zodiac = Console.ReadLine();
+
+            Console.Write("Кем вы работаете? : ");
+            job = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {name}, вам {age} лет, ваш знак зодиака {zodiac}, вы работаете {job}.");
+            Console.ReadKey();
+        }
+    }
+}
